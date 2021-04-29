274d51957d89cc89af9e9f16e9f48b29997e5782
swedishsailor <kzygowski7@gmail.com>
2021-04-29 18:25:21 +0200

    prompt input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-var computerMove;
-var playerMove;
-var randomNumber;
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień 2:papier 3:nożyce');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1'){
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+}else {
+    playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: '+ playerMove);
+
+
+// Computer moves
+
+var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 +1);
-computerMove = 'kamień';
-playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + playerMove + '! Jeśli twój ruch to kamień, to wygrywasz!');
-printMessage('Wylosowana livzba to: ' + randomNumber);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+}else {
+    computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
