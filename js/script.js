be0ed1c8c679b480f40582b32d629a54be2a7386
swedishsailor <kzygowski7@gmail.com>
2021-04-30 14:44:19 +0200

    Buttons added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-var playerMove, playerInput,computerMove, randomNumber, argMoveId, argPlayerMove, argComputerMove;
-playerInput = prompt('Wybierz swój ruch! 1: kamień 2:papier 3:nożyce');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-playerMove = getMoveName(playerInput);
-console.log('Ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 +1);
-console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('Ruch komputer to: ' + computerMove);
-displayResult(playerMove, computerMove);
+var playerMove, playerInput,computerMove, randomNumber, argMoveId, argPlayerMove, argComputerMove, buttonName, buttonRock, buttonPaper, buttonScissors;
+buttonRock = document.getElementById('button-rock');
+buttonRock.addEventListener('click', function(){buttonClicked('kamień');
+});
+buttonPaper = document.getElementById('button-paper');
+buttonPaper.addEventListener('click', function(){buttonClicked('papier');
+});
+buttonScissors = document.getElementById('button-scissors');
+buttonScissors.addEventListener('click', function(){buttonClicked('nożyce');
+});
 
 // Functions
 
@@ -37,4 +37,16 @@ function displayResult(argPlayerMove, argComputerMove) {
         printMessage('Przegrywasz');
     }
     printMessage('Zagrałem ' + argComputerMove + ', a ty ' + argPlayerMove);
+}
+
+function buttonClicked(buttonName) {
+    clearMessages();
+    console.log(buttonName + ' został kliknięty');
+    playerMove = buttonName;
+console.log('Ruch gracza to: ' + playerMove);
+randomNumber = Math.floor(Math.random() * 3 +1);
+console.log('wylosowana liczba to: ' + randomNumber);
+computerMove = getMoveName(randomNumber);
+console.log('Ruch komputer to: ' + computerMove);
+displayResult(playerMove, computerMove);
 }
